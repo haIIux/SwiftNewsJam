44d999f58d11b16d2c1a8a08db87d443e5088ba2
Rob Maltese <halluxdev@gmail.com>
2021-11-06 09:47:50 -0400

    link added, layout improved

--- SwiftNews/SwiftNews/RSSArticle/Views/RSSArticleView.swift
+++ SwiftNews/SwiftNews/RSSArticle/Views/RSSArticleView.swift
@@ -1,36 +1,40 @@
 import SwiftUI
 
 struct RSSArticleView: View {
-  let article: RSSArticle
-  
-  var body: some View {
-    VStack {
-      Text(article.title)
-        .font(.title)
-        .padding()
-      Spacer()
-      Text(article.content)
-        .font(.body)
-      Spacer()
+    let article: RSSArticle
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text(article.title)
+                    .bold()
+                    .font(.title2)
+                Link("Swift By Sundell", destination: URL(string: article.link)!)
+                    .font(.caption)
+                    .padding(.bottom, 5)
+                Text(article.content)
+                    .font(.body)
+            }
+            .padding(.horizontal)
+        }
     }
-  }
 }
 
 struct RSSArticleView_Preview: PreviewProvider {
-  static var previews: some View {
-    RSSArticleView(
-      article: RSSArticle(
-        id: .init(),
-        title: "Some RSS Article",
-        link: "https://swiftbysundell/podcast/108",
-        content: """
+    static var previews: some View {
+        RSSArticleView(
+            article: RSSArticle(
+                id: .init(),
+                title: "Some RSS Article",
+                link: "https://swiftbysundell/podcast/108",
+                content: """
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis cras sed felis eget velit aliquet sagittis id consectetur. Massa sapien faucibus et molestie ac feugiat sed lectus. In hendrerit gravida rutrum quisque non tellus. Et ligula ullamcorper malesuada proin libero nunc consequat. Viverra adipiscing at in tellus integer. A condimentum vitae sapien pellentesque habitant morbi. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus quam. Volutpat sed cras ornare arcu dui. Sapien eget mi proin sed libero enim sed faucibus. Faucibus turpis in eu mi bibendum neque egestas congue quisque.
                     
                     Et ultrices neque ornare aenean euismod. Viverra vitae congue eu consequat. Mauris a diam maecenas sed enim ut sem. Lobortis elementum nibh tellus molestie nunc non. Tincidunt id aliquet risus feugiat in ante metus. Ullamcorper morbi tincidunt ornare massa eget. Tortor dignissim convallis aenean et. Diam vulputate ut pharetra sit amet. Senectus et netus et malesuada fames. Eget aliquet nibh praesent tristique magna sit amet. Enim praesent elementum facilisis leo vel fringilla est ullamcorper. A diam sollicitudin tempor id eu.
-
+            
                     Hac habitasse platea dictumst quisque sagittis purus sit. Sagittis purus sit amet volutpat consequat mauris nunc congue nisi. Sed risus ultricies tristique nulla aliquet enim. At augue eget arcu dictum varius duis at. Id aliquet lectus proin nibh nisl condimentum id. Accumsan lacus vel facilisis volutpat est velit egestas. Elementum tempus egestas sed sed risus pretium quam vulputate. Odio tempor orci dapibus ultrices in iaculis nunc. Aenean et tortor at risus viverra adipiscing. Eget gravida cum sociis natoque. Enim facilisis gravida neque convallis a cras.
             """
-      )
-    )
-  }
+            )
+        )
+    }
 }
