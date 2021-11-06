738ff69218ae4efee8d0fc6e4b141cf956d3a781
Rob Maltese <halluxdev@gmail.com>
2021-11-05 21:01:41 -0400

    cleaned up feed builder

--- SwiftNews/SwiftNews/Services/FeedBuilder.swift
+++ SwiftNews/SwiftNews/Services/FeedBuilder.swift
@@ -7,38 +7,30 @@
 
 import Foundation
 
-/// Using protocol allows us to reuse the same setup for each feed we wish to include. This creates a `URLRequest` based off of our `path` which is appended to our `baseURL`. In our case, the `path` is typically always the same.
+/// Using protocol allows us to reuse the same setup for each feed we wish to include.  In our case, the `/rss` is typically always the same.
 protocol FeedBuilder {
-    var urlRequest: URLRequest { get }
     var baseURL: URL { get }
-    var path: String { get }
 }
 
 /// Using an enum to build out our desired feeds will allow for easier switching between feeds once we build in categories.
-/// Using our `FeedBuilder` we can take our `baseURL` and append it with our `path` in a more reusable fashion.
+/// Using our `FeedBuilder` we can take our `baseURL` and append it with  `/rss` in a more reusable fashion.
 ///
 /// - Parameter baseURL: Takes our `baseURL` that we decide to switch on which will be our feed host.
-/// - Parameter path: We will take our `path` and append it to our `baseURL` which in most cases, will be `/rss`.
 ///
 
 enum FeedURL {
     case sundell
+    case sarunw
 }
 
 extension FeedURL: FeedBuilder {
-
-    var urlRequest: URLRequest {
-        return URLRequest(url: self.baseURL.appendingPathComponent((self.path)))
-    }
-    
     var baseURL: URL {
         switch self {
         case .sundell :
-            return URL(string: "https://swiftbysundell.com")!
+            return URL(string: "https://swiftbysundell.com/rss")!
+        case .sarunw :
+            return URL(string: "https://sarunw.com/feed")!
         }
     }
 
-    var path: String {
-        return "/rss"
-    }
 }
