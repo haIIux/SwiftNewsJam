a20544db91fbc87ee60f8bd0a141ecda6ff39f23
Zach Eriksen <zmeriksen@icloud.com>
2021-11-07 13:11:26 -0700

    Fixed Timeline

--- SwiftNews/SwiftNewsWidget/SwiftNewsWidget.swift
+++ SwiftNews/SwiftNewsWidget/SwiftNewsWidget.swift
@@ -6,46 +6,87 @@
 //
 
 import WidgetKit
+import Combine
 import SwiftUI
-import ComposableArchitecture
-import SwiftyXML
 
-struct Provider: TimelineProvider {
+class Provider: TimelineProvider {
+    var bag = Set<AnyCancellable>()
+    
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
+        SimpleEntry(
+            date: Date(),
+            articles: [
+                RSSArticle(id: .init(), title: "Some RSS Article", description: "Blah Blah Blah", link: "some-link", pubDate: "Today", content: "...")
+            ]
+        )
     }
     
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
-        completion(entry)
+        guard !context.isPreview else {
+            completion(
+                SimpleEntry(
+                    date: Date(),
+                    articles: [
+                        RSSArticle(id: .init(), title: "Some RSS Article", description: "Blah Blah Blah", link: "some-link", pubDate: "Today", content: "...")
+                    ]
+                )
+            )
+            return
+        }
+        
+        FeedURL.sundell.fetch()
+            .sink(
+                receiveCompletion: { _ in },
+                receiveValue: { articles in
+                    completion(
+                        SimpleEntry(
+                            date: Date(),
+                            articles: articles
+                        )
+                    )
+                }
+            )
+            .store(in: &bag)
     }
     
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-        
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
-            entries.append(entry)
-        }
-        
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
+        FeedURL.sundell.fetch()
+            .sink(
+                receiveCompletion: { _ in },
+                receiveValue: { articles in
+                    var entries: [SimpleEntry] = []
+                    
+                    // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+                    let currentDate = Date()
+                    for hourOffset in 0 ..< 5 {
+                        let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+                        let entry = SimpleEntry(
+                            date: entryDate,
+                            articles: Array(articles.dropFirst(3 * hourOffset).prefix(3))
+                        )
+                        entries.append(entry)
+                    }
+                    
+                    let timeline = Timeline(entries: entries, policy: .atEnd)
+                    completion(timeline)
+                }
+            )
+            .store(in: &bag)
     }
 }
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
+    let articles: [RSSArticle]
 }
 
 struct SwiftNewsWidgetEntryView : View {
-    let store: Store<RSSFeed, RSSFeedAction>
     var entry: Provider.Entry
     
     var body: some View {
-        Text(entry.date, style: .time)
+        VStack {
+            Text(entry.articles.first?.title ?? "Loading...")
+        }
     }
 }
 
@@ -56,17 +97,6 @@ struct SwiftNewsWidget: Widget {
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             SwiftNewsWidgetEntryView(
-                store: Store(
-                    initialState: RSSFeed(
-                        id: .init(),
-                        title: "Swift by Sundell",
-                        articles: [],
-                        isFetchingData: false,
-                        feed: .sundell
-                    ),
-                    reducer: rssFeedReducer,
-                    environment: .live
-                ),
                 entry: entry
             )
         }
